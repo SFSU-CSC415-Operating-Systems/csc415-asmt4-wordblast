738d91a7f96a734da06f96d1fc821ac6c3f37835
student <mkim797@yahoo.com>
2022-10-01 17:34:44 -0700

    try to implement merge_sort

--- Kim_Mark_HW4_main.c
+++ Kim_Mark_HW4_main.c
@@ -20,7 +20,6 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
-#include <strings.h>
 
 #define INITIAL_ARRAY_SIZE  500
 #define QTY_TOP_WORDS        10
@@ -37,31 +36,33 @@ typedef struct
 typedef struct
 {
     word_freq *arr;
-    size_t used;
+    volatile size_t used;
     size_t size;
-} word_arr;
+} array;
 
 typedef struct
 {
     char *text;
-    volatile word_arr *words;
+    array *words;
     pthread_t thread_id;
     pthread_mutex_t mutex;
     int thread_num;
     int chunk_size;
 } thread_info;
 
-void init_array(volatile word_arr *words, size_t size);
+void init_array(array *words, size_t size);
 
-void insert_array(volatile word_arr *words, word_freq *word);
+void insert_array(array *words, char *token);
 
-void free_array(volatile word_arr *words);
+void free_array(array *words);
 
 void free_tinfo(thread_info *tinfo);
 
-void *test_array( volatile word_arr *words, char *text );
+void *test_array( array *words, char *text );
 
 void *process_chunk( void *arg );
+
+int max( int a, int b);
 
 int main (int argc, char *argv[])
 {
@@ -83,8 +84,8 @@ int main (int argc, char *argv[])
     FILE *in_file = fopen(argv[1], "r");
 
     // Initialize the word count array
-    volatile word_arr *word_freq_arr;
-    init_array(word_freq_arr, INITIAL_ARRAY_SIZE);
+    array *words = malloc(sizeof(array));
+    init_array(words, INITIAL_ARRAY_SIZE);
 
     // Check to see if fopen succeeds
     if (!in_file) 
@@ -106,14 +107,15 @@ int main (int argc, char *argv[])
         return EXIT_FAILURE;
     };
 
-    fread(buffer, sizeof(char), file_length, in_file);
+    size_t num_elements = fread(buffer, sizeof(char), file_length, in_file);
+    printf("Number of Elements: %lu\n", num_elements);
     fclose(in_file);
 
-    // pthread_mutex_t mutex;
-    // if (pthread_mutex_init(&mutex, NULL) != 0) {
-    //     perror("Error: mutex init failed.");
-    //     return EXIT_FAILURE;
-    // }
+    pthread_mutex_t mutex;
+    if (pthread_mutex_init(&mutex, NULL) != 0) {
+        perror("Error: mutex init failed.");
+        return EXIT_FAILURE;
+    }
 
     //**************************************************************
     // DO NOT CHANGE THIS BLOCK
@@ -126,46 +128,53 @@ int main (int argc, char *argv[])
     // *** TO DO ***  start your thread processing
     //                wait for the threads to finish
 
-    // thread_info *tinfo = (thread_info*)calloc(thread_count, sizeof(*tinfo));
-    // if (tinfo == NULL)
-    // {
-    //     perror("Error: calloc failed");
-    //     return EXIT_FAILURE;
-    // }
-
-    // for (int i = 0; i < thread_count; i++)
-    // {
-    //     tinfo[i].text = buffer;
-    //     tinfo[i].words = word_freq_arr;
-    //     tinfo[i].chunk_size = file_length / thread_count;
-    //     tinfo[i].thread_num = i;
-    //     tinfo[i].mutex = mutex;
-    //     if(pthread_create(&tinfo[i].thread_id, NULL, process_chunk, &tinfo[i]))
-    //     {
-    //         perror("Error: pthread_create failed");
-    //     };
-    // }
-
-    // for (int i = 0; i < thread_count; i++)
-    // {
-    //     if(pthread_join(tinfo[i].thread_id, &res))
-    //     {
-    //         perror("Error: pthread_join failed");
-    //         return EXIT_FAILURE;
-    //     };
-    // }
-    test_array(word_freq_arr, buffer);
-
-    printf("First 10 words in array");
-    for (int i = 0; i < word_freq_arr->used; i++) {
-        if (i < 10) {
-            printf("'%s': %d", word_freq_arr->arr[i].word, word_freq_arr->arr[i].freq);
-        }
+    thread_info *tinfo = (thread_info*)calloc(thread_count, sizeof(*tinfo));
+    if (tinfo == NULL)
+    {
+        perror("Error: calloc failed");
+        return EXIT_FAILURE;
     }
 
+    for (int i = 0; i < thread_count; i++)
+    {
+        tinfo[i].text = buffer;
+        tinfo[i].words = words;
+        tinfo[i].chunk_size = file_length / thread_count;
+        tinfo[i].thread_num = i;
+        tinfo[i].mutex = mutex;
+        if(pthread_create(&tinfo[i].thread_id, NULL, process_chunk, &tinfo[i]))
+        {
+            perror("Error: pthread_create failed");
+        };
+    }
+
+    for (int i = 0; i < thread_count; i++)
+    {
+        if(pthread_join(tinfo[i].thread_id, &res))
+        {
+            perror("Error: pthread_join failed");
+            return EXIT_FAILURE;
+        };
+    }
+
+    // char *lasts;
+    // char *token = strtok_r(buffer, delim, &lasts);
+    // for (int i = 0; i < 5; i++) {
+    //     insert_array(words, token);
+    //     printf("Token: '%s'\n  Array element: %d\n  word: '%s'\n  freq: %d\n\n", 
+    //         token, i, words->arr[i].word, words->arr[i].freq);
+    //     token = strtok_r(NULL, delim, &lasts);
+    // }
+
+    // printf("RESULTS TEST:\nUSED: %lu\nSIZE: %lu\n", words->used, words->size);
+    // // printf("'%s': %d", words->arr[0]->word, words->arr[0]->freq);
+    // for (int i = 0; i < 5; i++) {
+    //     printf("%d   '%s': %d\n", i, words->arr[i].word, words->arr[i].freq);
+    // }
+
     // ***TO DO *** Process TOP 10 and display
-    // word_arr *highest_counts = malloc(QTY_TOP_WORDS * sizeof(word_freq));
-    // word_freq_arr->size;
+    // array *highest_counts = malloc(QTY_TOP_WORDS * sizeof(word_freq));
+    // words->size;
 
     //**************************************************************
     // DO NOT CHANGE THIS BLOCK
@@ -184,69 +193,98 @@ int main (int argc, char *argv[])
 
 
     // ***TO DO *** cleanup
+    // free_tinfo(tinfo);
     free(buffer);
     buffer = NULL;
-    // free_tinfo(tinfo);
+    free_tinfo(tinfo);
+    pthread_mutex_destroy(&mutex);
 }
 
-void init_array(volatile word_arr *words, size_t size)
+void init_array(array *words, size_t size)
 {
-    words->arr = malloc(size, sizeof(char));
+    words->arr = malloc(size * sizeof(word_freq));
     words->used = 0;
     words->size = size;
 }
 
-void insert_array(volatile word_arr *words, word_freq *word)
+void insert_array(array *words, char *token)
 {
     if ( words->used == words->size ) {
         words->size *= 2;
         words->arr = realloc(words->arr, words->size * sizeof(word_freq));
     }
-    words->arr[words->used++].word = word;
+    word_freq new_word;
+    new_word.word = token;
+    new_word.freq = 1;
+    words->arr[words->used++] = new_word;
 };
 
-void free_array(volatile word_arr *words) 
+void free_array(array *words) 
 {
-    for (int i = 0; i < words->used; i++ ) {
-        free(words->arr[i].word);
-        words->arr[i].word = NULL;
-    }
     free(words->arr);
     words->arr = NULL;
     words->used = words->size = 0;
+    free(words);
+    words = NULL;
 };
 
 void free_tinfo(thread_info *tinfo)
 {
-    free(tinfo->text);
     tinfo->text = NULL;
     free_array(tinfo->words);
     tinfo->chunk_size = tinfo->thread_num = 0;
+    free(tinfo);
+    tinfo = NULL;
 }
 
-void *test_array( volatile word_arr *words, char *text )
-{
-    char *chunk = malloc(words->size/5);
-    chunk = strncpy(chunk, text, words->size/5);
-    char *lasts;
-    // Largest dictionary word is 49, so we initialize a byte size that is slightly greater
-    char *token = strtok_r(chunk, delim, &lasts);
-    while (token) {
-        if (strlen(token) > 5) {
-            word_freq *new_word = malloc(sizeof(word_freq));
-            new_word->word = malloc(strlen(token));
-            new_word->freq = 1;
-            if (words->used == words->size) {
-                words->size *= 2;
-                words->arr = realloc(words->arr, words->size * sizeof(word_freq));
-            }
-            words->arr[words->used++].word = new_word;
+word_freq* merge( array *words, int l, int m, int r ) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    word_freq left[n1], right[n2], ret_array[n1 + n2];
+
+    for (i = 0; i < n1; i++)
+        left[i] = words->arr[i];
+    for (j = 0; j < n2; j++)
+        right[j] = words->arr[m + 1 + j];
+
+    i = j = 0;
+    k = l;
+
+    while( i < n1 && j < n2)
+    {
+        if (left[i].freq <= right[j].freq) {
+            ret_array[k] = left[i];
+            i++;
+        } else {
+            ret_array[k] = right[j];
+            j++;
         }
-        token = strtok_r(NULL, delim, &lasts);
+        k++;
     }
-    free(chunk);
-    chunk = NULL;
-};
+
+    while (i < n1) {
+        ret_array[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        ret_array[k] = right[j];
+        j++;
+        k++;
+    }
+    
+    return ret_array;
+}
+
+word_freq* merge_sort( array *words, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        
+    }
+}
 
 void *process_chunk( void *arg )
 {
@@ -262,50 +300,39 @@ void *process_chunk( void *arg )
             char exists = 0;
             for (int i = 0; i < tinfo->words->used; i++)
             {
-                // {
-                //     exists = 1;
-                //     if(pthread_mutex_lock(&tinfo->mutex))
-                //     {
-                //         perror("Error: mutex lock failed.");
-                //     };
-                //     tinfo->words->arr[i].freq++;
-                //     if(pthread_mutex_unlock(&tinfo->mutex))
-                //     {
-                //         perror("Error: mutex failed to unlock");
-                //     };
-                // }
-                // if (!strcasecmp(tinfo->words->arr[i].word, token))
-                // {
-                //     exists = 1;
-                //     if(pthread_mutex_lock(&tinfo->mutex))
-                //     {
-                //         perror("Error: mutex lock failed.");
-                //     };
-                //     tinfo->words->arr[i].freq++;
-                //     if(pthread_mutex_unlock(&tinfo->mutex))
-                //     {
-                //         perror("Error: mutex failed to unlock");
-                //     };
-                // }
+                if (strcasecmp(tinfo->words->arr[i].word, token) == 0)
+                {
+                    exists = 1;
+                    if(pthread_mutex_lock(&tinfo->mutex))
+                    {
+                        perror("Error: mutex lock failed.");
+                    };
+                    tinfo->words->arr[i].freq++;
+                    if(pthread_mutex_unlock(&tinfo->mutex))
+                    {
+                        perror("Error: mutex failed to unlock");
+                    };
+                }
             }
             
-            // if (!exists) {
-            //     if (pthread_mutex_lock(&tinfo->mutex))
-            //     {
-            //         perror("Error: mutex lock failed.");
-            //     };
-            //     word_freq *new_word = malloc(sizeof(word_freq));
-            //     new_word->word = malloc(strlen(token));
-            //     new_word->freq = 1;
-            //     insert_array(tinfo->words, new_word);
-            //     if(pthread_mutex_unlock(&tinfo->mutex))
-            //     {
-            //         perror("Error: mutex failed to unlock");
-            //     };
-            // }
+            if (!exists) {
+                if (pthread_mutex_lock(&tinfo->mutex))
+                {
+                    perror("Error: mutex lock failed.");
+                };
+                insert_array(tinfo->words, token);
+                if(pthread_mutex_unlock(&tinfo->mutex))
+                {
+                    perror("Error: mutex failed to unlock");
+                };
+            }
         }
         token = strtok_r(NULL, delim, &lasts);
     }
     free(chunk);
     chunk = NULL;
 };
+
+int max( int a, int b) {
+    return (a > b) ? a : b;
+}
--- arraytest.c
+++ arraytest.c
@@ -23,50 +23,68 @@ typedef struct
 
 typedef struct
 {
-    word_freq *arr;
+    word_freq **arr;
     size_t used;
     size_t size;
 } word_arr;
 
-int init_array(volatile word_arr *words, size_t size);
+int init_array(word_arr *words, size_t size);
 
-void insert_array(volatile word_arr *words, word_freq *word);
+void insert_array(word_arr *words, word_freq *word);
 
-void free_array(volatile word_arr *words);
+void free_array(word_arr *words);
 
 int main( int argc, char *argv[])
 {
-    volatile word_arr *words;
+    word_arr *words = malloc(sizeof(word_arr));
     init_array(words, 10);
+    // word_freq *word = malloc(sizeof(word_freq));
+    // word->word = malloc(5);
+    // *word->word = "Hello";
+    // word->freq = 10;
+    // printf("'%s': %d", *word->word, word->freq);
+    // insert_array(words, word);
+    // printf("'%s': %d", *words->arr[0]->word, words->arr[0]->freq);
+    for (int i = 0; i < words->size; i++ ) {
+        word_freq *word = malloc(sizeof(word_freq));
+        word->word = malloc(5);
+        word->word = "Hello";
+        word->freq = i;
+        insert_array(words, word);
+    }
+    for (int i = 0; i < words->size; i++) {
+        printf("'%s': %d", words->arr[i]->word, words->arr[i]->freq);
+    }
+    return EXIT_SUCCESS;
 }
 
-int init_array(volatile word_arr *words, size_t size)
+int init_array(word_arr *words, size_t size)
 {
     words->arr = malloc(sizeof(words->arr) * size);
-    // if((words->arr = malloc(sizeof(word_freq) * size)) == NULL)
-    // {
-    //     perror("Error: init_array() failed -- malloc failed");
-    //     return EXIT_FAILURE;
-    // }
+    if((words->arr = malloc(sizeof(word_freq) * size)) == NULL)
+    {
+        perror("Error: init_array() failed -- malloc failed");
+        return EXIT_FAILURE;
+    }
     words->used = 0;
     words->size = size;
     return 0;
 }
 
-void insert_array(volatile word_arr *words, word_freq *word)
+void insert_array(word_arr *words, word_freq *word)
 {
     if ( words->used == words->size ) {
         words->size *= 2;
         words->arr = realloc(words->arr, words->size * sizeof(word_freq));
     }
-    words->arr[words->used++].word = word;
+    words->arr[words->used++] = word;
 };
 
-void free_array(volatile word_arr *words) 
+void free_array(word_arr *words) 
 {
     for (int i = 0; i < words->used; i++ ) {
-        free(words->arr[i].word);
-        words->arr[i].word = NULL;
+        free(words->arr[i]->word);
+        words->arr[i]->word = NULL;
     }
     free(words->arr);
     words->arr = NULL;
